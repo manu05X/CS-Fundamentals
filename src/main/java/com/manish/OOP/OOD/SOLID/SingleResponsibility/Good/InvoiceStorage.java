6acbbd112dc44aae10a8b3f663bd0b024ef9e64e
Manish Kumar <k.manu00005@gmail.com>
2024-09-02 12:28:37 +0530

    OOP -> OOD->SOLID->SRP

---
+++ src/main/java/com/manish/OOP/OOD/SOLID/SingleResponsibility/Good/InvoiceStorage.java
@@ -0,0 +1,9 @@
+package com.manish.OOP.OOD.SOLID.SingleResponsibility.Good;
+
+public
+class InvoiceStorage {
+    public void saveToDatabase(Invoice invoice) {
+        // Database-saving logic here
+        System.out.println("Invoice saved to the database.");
+    }
+}
---
+++ src/main/java/com/manish/OOP/OOD/SOLID/SingleResponsibility/OCP/InvoiceAction.java
@@ -0,0 +1,5 @@
+package com.manish.OOP.OOD.SOLID.SingleResponsibility.OCP;
+
+public interface InvoiceAction {
+    void perform(Invoice invoice);
+}
