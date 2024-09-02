01ca09edfdf292aeb124e18697d7a2cafb7c7e5e
Manish Kumar <k.manu00005@gmail.com>
2024-09-02 15:27:23 +0530

    Thread->Interview

---
+++ src/main/java/com/manish/Thread/Interview/BlockingQueue2/Faulty/FaultyBlockingQueueWithMutex.java
@@ -0,0 +1,58 @@
+package com.manish.Thread.Interview.BlockingQueue2.Faulty;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class FaultyBlockingQueueWithMutex<T> {
+    T[] array;
+    Lock lock = new ReentrantLock();
+    int size = 0;
+    int capacity;
+    int head = 0;
+    int tail = 0;
+
+    @SuppressWarnings("unchecked")
+    public FaultyBlockingQueueWithMutex(int capacity) {
+        // The casting results in a warning
+        array = (T[]) new Object[capacity];
+        this.capacity = capacity;
+    }
+
+    public T dequeue() {
+
+        T item = null;
+
+        while (size == 0) {
+        }
+
+        lock.lock();
+        if (head == capacity) {
+            head = 0;
+        }
+
+        item = array[head];
+        array[head] = null;
+        head++;
+        size--;
+
+        lock.unlock();
+        return item;
+    }
+
+    public void enqueue(T item) {
+
+        while (size == capacity) {
+        }
+
+        lock.lock();
+        if (tail == capacity) {
+            tail = 0;
+        }
+
+        array[tail] = item;
+        size++;
+        tail++;
+        lock.unlock();
+    }
+}
---
+++ src/main/java/com/manish/Thread/Interview/BlockingQueue3/EX1/CountingSemaphore.java
@@ -0,0 +1,34 @@
+package com.manish.Thread.Interview.BlockingQueue3.EX1;
+
+public class CountingSemaphore {
+
+    int usedPermits = 0;
+    int maxCount;
+
+    public CountingSemaphore(int count) {
+        this.maxCount = count;
+    }
+
+    public CountingSemaphore(int count, int initialPermits) {
+        this.maxCount = count;
+        this.usedPermits = this.maxCount - initialPermits;
+    }
+
+    public synchronized void acquire() throws InterruptedException {
+
+        while (usedPermits == maxCount)
+            wait();
+
+        notify();
+        usedPermits++;
+    }
+
+    public synchronized void release() throws InterruptedException {
+
+        while (usedPermits == 0)
+            wait();
+
+        usedPermits--;
+        notify();
+    }
+}
