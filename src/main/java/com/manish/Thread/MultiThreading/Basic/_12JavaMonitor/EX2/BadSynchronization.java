f05d8c2e4021202c124d841521463152e3a37391
Manish Kumar <k.manu00005@gmail.com>
2024-09-02 14:18:41 +0530

    Thread->Multithread->Basic->_15

---
+++ src/main/java/com/manish/Thread/MultiThreading/Basic/_12JavaMonitor/EX2/BadSynchronization.java
@@ -0,0 +1,17 @@
+package com.manish.Thread.MultiThreading.Basic._12JavaMonitor.EX2;
+
+class BadSynchronization {
+
+    public static void main(String args[]) {
+        Object dummyObject = new Object();
+        Object lock = new Object();
+
+        synchronized (lock) {
+            lock.notify();
+
+            // Attempting to call notify() on the object
+            // in synchronized block of another object
+            dummyObject.notify();
+        }
+    }
+}
