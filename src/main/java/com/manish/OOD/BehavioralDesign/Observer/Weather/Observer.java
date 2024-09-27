b8ba9e6b02108b0bd55ae870cced9834373bf414
Manish Kumar <k.manu00005@gmail.com>
2024-09-27 15:54:12 +0530

    Adding the collectio

---
+++ src/main/java/com/manish/OOD/BehavioralDesign/Observer/Weather/Observer.java
@@ -0,0 +1,6 @@
+package com.manish.OOD.BehavioralDesign.Observer.Weather;
+
+// Observer Interface
+interface Observer {
+    void update(String weather);
+}
---
+++ src/main/java/com/manish/OOD/BehavioralDesign/Observer/Weather/PhoneDisplay.java
@@ -0,0 +1,16 @@
+package com.manish.OOD.BehavioralDesign.Observer.Weather;
+
+// ConcreteObserver Class
+class PhoneDisplay implements Observer {
+    private String weather;
+
+    @Override
+    public void update(String weather) {
+        this.weather = weather;
+        display();
+    }
+
+    private void display() {
+        System.out.println("Phone Display: Weather updated - " + weather);
+    }
+}
